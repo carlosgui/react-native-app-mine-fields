125a4a4ca1dee7d06f08250278a2b8d6acaba159
carlos.guimaraes <carlosh.guimaraes@hotmail.com>
2021-03-26 23:22:29 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+import params from './src/params'
+import { createMinedBoard } from './src/functions'
+import MineField from './src/components/MineField';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = this.createState()
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return {
+      board: createMinedBoard(cols, rows, this.minesAmount()),
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.board}>
+          <MineField board={this.state.board} />
+        </View>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
   },
+  board: {
+    alignItems: 'center',
+    backgroundColor: '#AAA'
+  }
 });
---
+++ src/components/MineField.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+import Field from './Field'
+
+export default props => {
+    const rows = props.board.map((row, r) => {
+        const columns = row.map((fields, c) => {
+            return <Field  {...fields} key={c}/>
+        })
+        return <View key={r} style={styles.columns}>{columns}</View>
+    })
+    return <View style={styles.container}>{rows}</View>
+}
+
+const styles = StyleSheet.create({
+    container: {
+      backgroundColor: '#EEE'
+    },
+    columns: {
+      flexDirection: 'row',
+    }
+})
